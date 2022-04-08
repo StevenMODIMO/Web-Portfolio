df5bdb3dad91d7cde0c7023d786a565d504d3e8f
Esteban <stevepetes26@gmail.com>
2022-04-08 12:51:34 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const icon = document.querySelector('.ic');
+
+
+icon.addEventListener('click', function(){
+    const links = document.querySelector('.links')
+    links.classList.toggle('change-links')
+})
