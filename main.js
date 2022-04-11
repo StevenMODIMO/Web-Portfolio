7371ac83b4db40fad49dec9ee0c7804567826a75
Esteban <stevepetes26@gmail.com>
2022-04-11 10:41:53 -0400

    Initial Commit

--- main.js
+++ main.js
@@ -4,4 +4,23 @@ const icon = document.querySelector('.ic');
 icon.addEventListener('click', function(){
     const links = document.querySelector('.links')
     links.classList.toggle('change-links')
-})
+});
+
+
+const btn = document.getElementById('btn')
+
+window.onscroll = () => scrollFunction();
+
+const scrollFunction = () => {
+    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        btn.style.display = 'block'
+    } else {
+        btn.style.display = 'none'
+    }
+}
+
+btn.addEventListener('click', () => window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+}))
