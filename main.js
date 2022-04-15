77a7ad958edcf961cd952a25d015d29f7f8ff491
Esteban <stevepetes26@gmail.com>
2022-04-15 13:29:44 -0400

    Initial Commit

--- main.js
+++ main.js
@@ -19,8 +19,45 @@ const scrollFunction = () => {
     }
 }
 
-btn.addEventListener('click', () => window.scroll({
+btn.addEventListener('click', () =>  window.scroll({
     top: 0,
     left: 0,
     behavior: 'smooth'
-}))
+}));
+
+const main = document.getElementById('page');
+
+function slide() {
+    main.classList.add('show')
+}
+
+document.body.onload = setTimeout(slide, 1000)
+
+document.body.onload = setTimeout(showPage, 1000);
+ 
+
+const showContact = document.querySelector('.form');
+function showPage() {
+    showContact.classList.add('show-form')
+};
+
+
+/**Start of Email JS  */
+(function () {
+    emailjs.init("MWzXJKDDEzAjLrrPt");
+  })();
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const templateParams = {
+       from_name : document.getElementById('name-info').value,
+       email_id : document.getElementById('email-info').value,
+       message : document.getElementById('sms').value
+    }
+
+    emailjs.send('service_vwdkg3s', 'template_yrfposg', templateParams).then(() => {
+         alert('Success 200')
+       })
+});
+
+/**End of Email JS */
