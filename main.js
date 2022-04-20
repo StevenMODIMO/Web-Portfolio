bc499aebdba28938d27d0b71a695c903cd7c20e8
Esteban <stevepetes26@gmail.com>
2022-04-20 01:51:19 -0400

    Initial Commit

--- main.js
+++ main.js
@@ -1,63 +1,51 @@
-const icon = document.querySelector('.ic');
-
-
-icon.addEventListener('click', function(){
-    const links = document.querySelector('.links')
-    links.classList.toggle('change-links')
+/**Start Of Show Link Button */
+const showLinkButton = document.querySelector(".con");
+//console.log(showLinkButton);
+showLinkButton.addEventListener("click", function () {
+  const links = document.querySelector(".links");
+  links.classList.toggle("toggle-links");
 });
+/**End Of Show Link Button */
 
+/**Start Of Main Page*/
+const introPage = document.querySelector(".intro");
+const showIntroPage = () => {
+  introPage.classList.add("show-intro");
+};
+document.body.onload = setTimeout(showIntroPage, 2000);
+/**End Of Main Page */
 
-const btn = document.getElementById('btn')
+/**Start of Client Page */
+const clientPage = document.querySelector(".client-to-me");
+const showClientPage = () => {
+  clientPage.classList.add("show-client");
+};
+document.body.onload = setTimeout(showClientPage, 3000);
+/**End Of Client Page */
+
+/**Start Of Top Button */
+const topButton = document.querySelector("#top-btn");
+//console.log(topButton);
 
 window.onscroll = () => scrollFunction();
 
 const scrollFunction = () => {
-    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        btn.style.display = 'block'
-    } else {
-        btn.style.display = 'none'
-    }
-}
-
-btn.addEventListener('click', () =>  window.scroll({
-    top: 0,
-    left: 0,
-    behavior: 'smooth'
-}));
-
-const main = document.getElementById('page');
-
-function slide() {
-    main.classList.add('show')
-}
-
-document.body.onload = setTimeout(slide, 1000)
-
-document.body.onload = setTimeout(showPage, 1000);
- 
-
-const showContact = document.querySelector('.form');
-function showPage() {
-    showContact.classList.add('show-form')
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
 };
 
+topButton.onclick = () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
+/**End Of top Button */
 
-/**Start of Email JS  */
-(function () {
-    emailjs.init("MWzXJKDDEzAjLrrPt");
-  })();
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const templateParams = {
-       from_name : document.getElementById('name-info').value,
-       email_id : document.getElementById('email-info').value,
-       message : document.getElementById('sms').value
-    }
-
-    emailjs.send('service_vwdkg3s', 'template_yrfposg', templateParams).then(() => {
-         alert('Success 200')
-       })
-});
-
-/**End of Email JS */
